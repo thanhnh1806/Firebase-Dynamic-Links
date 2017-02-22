56b8a2ec28444922da6cc3d2158d504f2e21ccf7
Thanh Nguyen <thanhnh1806@gmail.com>
2017-02-22 12:00:54 +0700

    Create Project

---
+++ app/src/main/java/com/example/thanhnguyen/firebase_dynamic_links/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.thanhnguyen.firebase_dynamic_links;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
